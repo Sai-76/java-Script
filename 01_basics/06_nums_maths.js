8e28ee839ff692e6afc42935bf103307ceecf2f4
Sai-76 <142737664+Sai-76@users.noreply.github.com>
2025-07-20 20:07:01 +0000

    06_nums_maths.js

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,28 @@
+// const score = 400;
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+// console.log(score)
+
+// const othernum = 123.456789
+
+// console.log(othernum.toPrecision(4))
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'))
+// console.log(hundreds.toLocaleString())
+// console.log(Number.MAX_INTEGER) //undefined
+// console.log(Number.MIN_INTEGER) // undefined
+// console.log(Number.MAX_SAFE_INTEGER) //9007199254740991
+/* ------------------------------ MATH ----------------------------- */
+
+// console.log(Math)
+// console.log(Math.PI)
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(4.12))
+// console.log(Math.floor(4.876))
+// console.log(Math.sign(4.876))
+// console.log(Math.tanh(45))
+console.log(Math.random()*10+1)
+// random - gives value between 0 and 1 
